aaf23b61ecb015f3903f4dec0ab52451bed06ae1
robincollins <racollins1@me.com>
2018-01-19 23:30:49 +0000

    created journey class and created first test

---
+++ spec/journey_spec.rb
@@ -0,0 +1,18 @@
+require "journey"
+require "car"
+
+RSpec.describe Journey do
+
+  let(:start_location) { double :start_location }
+  let(:finish_location) { double :finish_location }
+
+  subject(:journey) { described_class.new }
+
+
+  it "collates the start location and end location" do
+    car.fill_up
+    car.drive(start_location)
+    car.drive(finish_location)
+    expect(car.journey).to eq {[start_location], [finish_location]}
+  end
+end
