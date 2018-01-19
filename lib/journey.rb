214135523555b187723d2181764b012820c7cfb8
robincollins <racollins1@me.com>
2018-01-19 23:43:17 +0000

    created an instance of journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,4 +2,8 @@ require_relative "car"
 
 class Journey
 
+  def complete_trip
+  end
+
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -9,11 +9,14 @@ RSpec.describe Journey do
   subject(:journey) { described_class.new }
   subject(:car) { Car.new }
 
+  it "exists" do
+    expect(journey).to be_an_instance_of Journey
+  end
 
   it "collates the start location and end location" do
     car.fill_up
     car.drive(start_location)
     car.drive(finish_location)
-    expect(car.journey).to eq {[start_location] [finish_location]}
+    expect(journey.complete_trip).to eq {[start_location] [finish_location]}
   end
 end
